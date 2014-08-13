f94fe19c4ec69fcf8843939cd458d7e0ea49f1fc
Seb Dangerfield <seb.dangerfield@capablue.com>
2014-08-13 10:57:04 +0100

    Setting permissiosn to allow activemq to run as non-privileged user (inc. wrapper)

--- manifests/local_install.pp
+++ manifests/local_install.pp
@@ -14,7 +14,8 @@ class activemq::local_install (
   $download_url = 'http://apache.mirrors.lucidnetworks.net/activemq/5.10.0/apache-activemq-5.10.0-bin.tar.gz',
   $install_dir  = '/opt',
   $wrapper_cmd  = '/opt/activemq/bin/linux-x86-64/wrapper',
-  $wrapper_conf = '/opt/activemq/bin/linux-x86-64/wrapper.conf'
+  $wrapper_conf = '/opt/activemq/bin/linux-x86-64/wrapper.conf',
+  $pid_dir      = '/opt/activemq/data'
 ) {
 
   user { 'activemq':
@@ -41,6 +42,20 @@ class activemq::local_install (
     cwd => '/tmp',
     unless => 'ls /opt/*activemq*',
     path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin'
+  } ->
+  file { 'activemq-dir-root':
+    ensure => directory,
+    path => '/opt/activemq',
+    recurse => true,
+    owner => 'root',
+    group => 'root'
+  } ->
+  file { 'activemq-data-dir':
+    ensure => directory,
+    path => '/opt/activemq/data',
+    recurse => true,
+    owner => 'activemq',
+    group => 'activemq'
   }
 
   # Manually adding the init script as this won't get added automatically
