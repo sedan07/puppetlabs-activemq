4d3316c45c72c75b52fd5b35321d980e45254850
Seb Dangerfield <seb.dangerfield@capablue.com>
2015-06-25 11:17:35 +0100

    Ensure users are not duplicated and either added or updated

--- manifests/config/jetty_realm.pp
+++ manifests/config/jetty_realm.pp
@@ -3,18 +3,25 @@
 define activemq::config::jetty_realm (
   $username,
   $password,
-  $realm,
-  $file
+  $file,
+  $realms = [],
 ){
   validate_re($username, '^(?=.*[a-zA-Z0-9]$)[a-zA-Z0-9][a-zA-Z0-9_-]*$', 'The Jetty Realm username should only use a-z 0-9 and - and _ chars')
 
-  augeas { "activemq_jetty_realm_${username}":
+  $realm_list = join($realms, ",")
+
+  $path_exact = "[ username = \"${username}\" and password = \"${password}\" and realm = \"${realm_list}\" ]"
+  $path_list = "username[.=\"${username}\"]"
+
+  augeas { "activemq/jetty-realm/${username}":
     incl    => $file,
     lens    => 'JettyRealm.lns',
     changes => [
-      "set user[last()]/username ${username}",
+      "rm ${path_list}",
+      "set user[last()+1]/username ${username}",
       "set user[last()]/password ${password}",
-      "set user[last()]/realm ${realm}"
+      "set user[last()]/realm ${realm_list}"
     ],
+    onlyif  => "match user[*]${path_exact} size == 0",
   }
 }
