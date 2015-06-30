5d333cdf9535fc5521a4f193dfb8e7ad2313f902
Seb Dangerfield <seb.dangerfield@capablue.com>
2015-06-30 13:53:03 +0100

    Adding credentials file manifest for changing default admin login

---
+++ manifests/config/credentials.pp
@@ -0,0 +1,23 @@
+# Class: activemq::config::credentials
+#
+class activemq::config::credentials (
+  $activemq_username = 'system',
+  $activemq_password = 'manager',
+  $guest_password    = 'password',
+  $file,
+){
+  $activemq_username_list = 'activemq.username'
+  $activemq_password_list = 'activemq.password'
+  $guest_password_list = 'guest.password'
+
+  augeas { 'activemq/credentials':
+    incl    => $file,
+    lens    => 'Properties.lns',
+    changes => [
+      "set ${activemq_username_list} ${activemq_username}",
+      "set ${activemq_password_list} ${activemq_password}",
+      "set ${guest_password_list} ${guest_password}",
+    ],
+  }
+
+}
