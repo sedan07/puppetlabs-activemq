15c3b53ff58cf75fa588fcc6c6e4b4e7e2420db1
Seb Dangerfield <seb.dangerfield@capablue.com>
2015-07-01 12:01:24 +0100

    Adding test for adding multiple realms to spec for jetty_realms

--- spec/defines/config/jetty_realm_spec.rb
+++ spec/defines/config/jetty_realm_spec.rb
@@ -2,21 +2,44 @@ require 'spec_helper'
 
 describe 'activemq::config::jetty_realm' do
   let(:title) { 'test' }
-  let(:params) { {
-    :username => 'demo',
-    :password => 'guessme',
-    :file     => '/etc/activemq/jetty-realm.properties',
-    :realms   => [ 'guest' ]
-  } }
 
-  it { should contain_augeas('activemq/jetty-realm/demo').with({
-    'incl' => '/etc/activemq/jetty-realm.properties',
-    'lens' => 'JettyRealm.lns',
-    'changes' => [
-      'rm user[*][ username = "demo" ]',
-      'set user[last()+1]/username demo',
-      'set user[last()]/password guessme',
-      'set user[last()]/realm guest'
-    ]
-  })}
+  describe 'user with a single realm' do
+    let(:params) { {
+      :username => 'demo',
+      :password => 'guessme',
+      :file     => '/etc/activemq/jetty-realm.properties',
+      :realms   => [ 'guest' ]
+    } }
+
+    it { should contain_augeas('activemq/jetty-realm/demo').with({
+      'incl' => '/etc/activemq/jetty-realm.properties',
+      'lens' => 'JettyRealm.lns',
+      'changes' => [
+        'rm user[*][ username = "demo" ]',
+        'set user[last()+1]/username demo',
+        'set user[last()]/password guessme',
+        'set user[last()]/realm guest'
+      ]
+    })}
+  end
+
+  describe 'user with multiple realms' do
+    let(:params) { {
+      :username => 'demo',
+      :password => 'guessme',
+      :file     => '/etc/activemq/jetty-realm.properties',
+      :realms   => [ 'guest', 'admin' ]
+    } }
+
+    it { should contain_augeas('activemq/jetty-realm/demo').with({
+      'incl' => '/etc/activemq/jetty-realm.properties',
+      'lens' => 'JettyRealm.lns',
+      'changes' => [
+        'rm user[*][ username = "demo" ]',
+        'set user[last()+1]/username demo',
+        'set user[last()]/password guessme',
+        'set user[last()]/realm guest,admin'
+      ]
+    })}
+  end
 end
